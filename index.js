d6f19e434a554b573cf1016c1da34de58bb2f7cb
Shima Naderi <shima.naderii@gmail.com>
2022-12-05 23:16:58 +0100

    try with javascript

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let shoppingCart = {};
 const products = document.getElementsByClassName("add-to-the-card");
-
+/* const addButtons = document.getElementsByClassName("quantity-button")
+*/
 Array.from(products).forEach(product => product.addEventListener("click", event => {
     addToTheCart(event)
 }))
@@ -23,14 +24,27 @@ const setCart = object => {
 if(shoppingCart.hasOwnProperty(product.id)) {
     
     shoppingCart[product.id].productQuantity = product.productQuantity;
-    /* console.log(shoppingCart); */
+    console.log(shoppingCart); 
 
 }else {
     shoppingCart[product.id] = {...product}
-    /*console.log(shoppingCart);*/
+    console.log(shoppingCart);
 }
-
-
 };
 
-
+document.querySelectorAll(".quantity-button").forEach((qbutton) => {
+    qbutton.addEventListener("click", () => {
+        /* if (e.target.classList.contains("quantity-button")) {
+           const product = shoppingCart[e.target.dataset.textContent]
+           product.productQuantity = shoppingCart[e.target.dataset.textContent].productQuantity+1 
+        } */
+        const type = qbutton.getAttribute("data-type");
+        /* const currentQuantity = parseInt(input.value); */
+        const product = shoppingCart[product.id];
+        if(type === "substract" && productQuantity> 0){
+            product.productQuantity = productQuantity-1
+        } else if (type === "add"){
+            product.productQuantity = productQuantity+1
+        }
+    } )
+}) 
