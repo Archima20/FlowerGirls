05acb6d12626cb3ce4accbc6a4ea71ca56c6b6a4
Liliana <lilianadalmarco@gmail.com>
2022-12-07 15:56:40 +0100

    avances de javaScript

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const setCart = (object) => {
     shoppingCart[product.id] = { ...product };
     console.log(shoppingCart);
   }
-  localStorage.setItem('shoppingcart', JSON.stringify(shoppingCart));
+  sessionStorage.setItem('shoppingcart', JSON.stringify(shoppingCart));
 };
 
 document.querySelectorAll(".quantity-button").forEach((qbutton) => {
@@ -46,4 +46,46 @@ document.querySelectorAll(".quantity-button").forEach((qbutton) => {
   });
 });
 
+document.addEventListener('DOMContentLoaded', ( )=> {
+    if (sessionStorage.getItem('shoppingcart')) {
+      shoppingCart = JSON.parse(sessionStorage.getItem('shoppingcart'));
+      printShoppingCart()
+    }
+  })
+  
+  const templateshoppingcart = document.getElementById("templateShoppingCart").content;
+  const fragment = document.createDocumentFragment();
+  
+  const printShoppingCart = () => {
+      Object.values(shoppingCart).forEach(product =>{
+          templateshoppingcart.querySelector('#selectedProduct').value = product.value;
+          templateshoppingcart.querySelector('img').src = product.image;
+          templateshoppingcart.querySelector('p').textContent = product.productName;
+          templateshoppingcart.querySelector('span').textContent = product.productPrice;
+          templateshoppingcart.querySelector('.btn-substract').dataset.type = product.type;
+          templateshoppingcart.querySelector('input').value = product.productQuantity;
+          templateshoppingcart.querySelector('.btn-add').dataset.type = product.type;
+          const clone = templateshoppingcart.cloneNode(true);
+          fragment.appendChild(clone);
+      })  
+      items.appendChild(fragment);
+  
+  const  totalQuantity = Object.values(shoppingCart).reduce((acc, {productQuantity}) => acc + productQuantity, 0)
 
+  
+  const  totalPrice = Object.values(shoppingCart).reduce((acc, {productQuantity, productPrice}) => acc + productQuantity * productPrice, 0)
+  
+  const total = document.querySelector(".total-price")
+    
+  total.innerHTML = `${totalPrice}`
+  
+  const emptyCartButton = document.querySelector(".empty-car");
+  console.log(emptyCartButton);
+  emptyCartButton.addEventListener("click", () => {
+    shoppingCart = {};
+    sessionStorage.clear();
+    fetch();
+
+
+  });
+}
